0c2e3c8faacaa63d83d6badb587e7f41234f0d1e
SebastienBtr <sebastien29520@gmail.com>
2020-03-18 16:36:51 +0100

    add  api versionning

--- microservices/article/src/routes.js
+++ microservices/article/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const routesVersioning = require('express-routes-versioning')();
 
 const router = express.Router();
 const ctrl = require('./controllers');
@@ -11,26 +12,36 @@ router.get('/health', (req, res) => res.status(200).send());
 /**
  * Get all the articles
  */
-router.get('/articles', ctrl.getAllArticles);
+router.get('/articles', routesVersioning({
+  '^1.0.0': ctrl.getAllArticles,
+}));
 
 /**
  * Create an article
  */
-router.post('/articles', ctrl.createArticle);
+router.post('/articles', routesVersioning({
+  '^1.0.0': ctrl.createArticle,
+}));
 
 /**
  * Update an article
  */
-router.put('/articles/:id', ctrl.updateArticle);
+router.put('/articles/:id', routesVersioning({
+  '^1.0.0': ctrl.updateArticle,
+}));
 
 /**
  * Get an article by id
  */
-router.get('/articles/:id', ctrl.getArticleById);
+router.get('/articles/:id', routesVersioning({
+  '^1.0.0': ctrl.getArticleById,
+}));
 
 /**
  * Delete an article
  */
-router.delete('/articles/:id', ctrl.deleteArticle);
+router.delete('/articles/:id', routesVersioning({
+  '^1.0.0': ctrl.deleteArticle,
+}));
 
 module.exports = router;
