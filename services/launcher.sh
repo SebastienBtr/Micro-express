437ba18310c8ffa4747dc03732930774705f38a1
SebastienBtr <sebastien29520@gmail.com>
2020-03-19 23:39:15 +0100

    edit launcher

--- services/launcher.sh
+++ services/launcher.sh
@@ -2,18 +2,6 @@
 scriptArguments=$@
 root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Check if the script arguments contains a given value
-containsElement() {
-  for var in $scriptArguments
-  do 
-    if [ $1 == $var ]
-    then
-      return 0
-    fi
-  done
-  return 1
-}
-
 launchAll() {
   # TODO: lauch api gateway
   cd "$root"/kafka
@@ -28,27 +16,10 @@ if test "$#" -lt 1
 then
   launchAll
 else
-  if containsElement api-gateway
-  then
-    echo "gateway"
-    # TODO: lauch api gateway
-  fi
-    if containsElement kafka
-  then
-    echo "kafka"
-    cd "$root"/kafka
+  for var in $scriptArguments
+  do 
+    echo "Launch " $var
+    cd "$root"/$var
     docker-compose up -d
-  fi
-    if containsElement article
-  then
-    echo "article"
-    cd "$root"/article
-    docker-compose up -d
-  fi
-  if containsElement cart
-  then
-    echo "cart"
-    cd "$root"/cart
-    docker-compose up -d
-  fi
+  done
 fi
