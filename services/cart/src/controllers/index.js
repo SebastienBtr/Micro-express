fc845cd71d6aca2a7ee161d8369b747eaa775b99
SebastienBtr <sebastien29520@gmail.com>
2020-03-23 10:19:39 +0100

    add routes in cart service

--- services/cart/src/controllers/index.js
+++ services/cart/src/controllers/index.js
@@ -1,1 +1,5 @@
 exports.getAllCartItems = require('./getAllCartItemsCtrl').getAllCartItems;
+exports.checkout = require('./checkoutCtrl').checkout;
+exports.createCartItem = require('./createCartItemCtrl').createCartItem;
+exports.deleteCartItem = require('./deleteCartItemCtrl').deleteCartItem;
+exports.updateCartItemQuantity = require('./updateCartItemQuantityCtrl').updateCartItemQuantity;
--- services/cart/src/routes.js
+++ services/cart/src/routes.js
@@ -16,4 +16,33 @@ router.get('/cart/items', routesVersioning({
   '^1.0.0': ctrl.getAllCartItems,
 }));
 
+/**
+ * Add an item in the cart
+ */
+router.post('/cart/items', routesVersioning({
+  '^1.0.0': ctrl.createCartItem,
+}));
+
+/**
+ * Delete an item of the cart
+ */
+router.delete('/cart/items/:id', routesVersioning({
+  '^1.0.0': ctrl.deleteCartItem,
+}));
+
+/**
+ * Update the quantity of an item of the cart
+ */
+router.put('/cart/items/:id/quantity', routesVersioning({
+  '^1.0.0': ctrl.updateCartItemQuantity,
+}));
+
+/**
+ * Checkout the items for the payment
+ * (in our POC this will update article's stocks and empty the cart)
+ */
+router.put('/cart/checkout', routesVersioning({
+  '^1.0.0': ctrl.checkout,
+}));
+
 module.exports = router;
