5675778c33ad4cee6adb48162da04632450ba6c9
SebastienBtr <sebastien29520@gmail.com>
2020-04-01 14:24:44 +0200

    add env variable to the launcher

--- services/launcher.sh
+++ services/launcher.sh
@@ -1,33 +1,54 @@
 #!/bin/bash
 source ~/.bashrc # to access "docker-compose" alias in production
-scriptArguments=$@
 root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+env="dev"
+declare -a scriptArguments
+
+# process arguments
+while [[ $# -gt 0 ]]
+do
+    case "$1" in
+      -e)
+      if [[ $2 != "" ]]; then env=$2; fi
+      shift 2
+      ;;
+      *)
+      scriptArguments+=( $1 )
+      shift 1
+      ;;
+    esac
+done
 
 launchAll() {
   cd "$root"/api-gateway
-  docker-compose up -d --build
+  ENV=$env docker-compose up -d --build --force-recreate
   cd "$root"/kafka
-  docker-compose up -d --build
+  ENV=$env docker-compose up -d --build --force-recreate
   cd "$root"/article
-  docker-compose up -d --build
+  ENV=$env docker-compose up -d --build --force-recreate
   cd "$root"/cart
-  docker-compose up -d --build
+  ENV=$env docker-compose up -d --build --force-recreate
   cd "$root"/documentation
-  docker-compose up -d --build
+  ENV=$env docker-compose up -d --build --force-recreate
 }
 
-if test "$#" -lt 1
+if test ${#scriptArguments[@]} -lt 1
 then
+  echo "Launch all"
   launchAll
 else
   cd "$root"/api-gateway
-  docker-compose up -d --build
-  cd "$root"/kafka
-  docker-compose up -d --build
+  ENV=$env docker-compose up -d --build --force-recreate
+  if [[ $env != "prod" ]]
+  then
+    cd "$root"/kafka
+    ENV=$env docker-compose up -d --build --force-recreate
+  fi
   for var in $scriptArguments
   do 
     echo "Launch " $var
     cd "$root"/$var
-    docker-compose up -d --build
+    ENV=$env docker-compose up -d --build --force-recreate
   done
 fi
