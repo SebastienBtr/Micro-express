fa79ba46a4ea45769b68cccc447dac638225a42c
SebastienBtr <sebastien29520@gmail.com>
2020-06-17 09:42:25 +0200

    edit stop script

--- services/stop-all.sh
+++ services/stop-all.sh
@@ -3,11 +3,11 @@ source ~/.bashrc # to access "docker-compose" alias in production
 root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "$root"/article
-docker-compose down
+docker-compose -f docker-compose.yaml -f docker-compose.local.yaml -f docker-compose.hot-reload.yaml down
 cd "$root"/cart
-docker-compose down
+docker-compose -f docker-compose.yaml -f docker-compose.local.yaml -f docker-compose.hot-reload.yaml down
 cd "$root"/documentation
-docker-compose down
+docker-compose -f docker-compose.yaml -f docker-compose.local.yaml -f docker-compose.hot-reload.yaml down
 cd "$root"/api-gateway
 docker-compose down
 cd "$root"/kafka
